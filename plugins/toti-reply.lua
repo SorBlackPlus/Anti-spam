c1f4cde8312e184b3df71c8b4f0e69fd1faf390e
SorBlackPlus <sorblackplus@gmail.com>
2017-03-19 06:42:58 -0700

    Add Plugin Toti-Reply By #SorBlack :):|

---
+++ plugins/toti-reply.lua
@@ -0,0 +1,19 @@
+local function run(msg, matches)
+  local text = matches[1]
+  return text
+  end 
+
+  return {
+   description = "reply msg.",
+   usage = {
+      "reply [msg]"
+   },
+   patterns = {
+      "^(.*)$"
+   },
+   run = run,
+   hide = true
+}
+
+--by #SorBlack :):|
+--channel: @PrimeTeam
